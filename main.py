a795bd25848e3dc606622f5722e2908423fb2e87
KrozFu <carlosuefagh@gmail.com>
2026-03-17 17:01:25 -0500

    chore: creating artifacts with python, core of the process

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-user = "admin@uniremington.edu.co"
-password = "admin123"
+user = "admin@admin.co"
+password = "1234"
 
-user_input = input()
-pass_input = input()
+user_input = input("Ingrese el usuario: ")
+password_input = input("Ingrese contraseña: ")
 
+if user_input == user and password_input == password:
+    input("Sesion inciada paulatinamente")
+else:
+    input("usuario o contraseña incorrecta")
